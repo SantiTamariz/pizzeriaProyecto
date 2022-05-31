f81d565b17021b658e3128a39ceef6e194d9d070
Stmarco <S.tmarco@alumnos.upm.es>
2022-05-31 13:37:23 +0200

    Estado inicial

---
+++ src/main/java/com/santiago/pizzeriaProyecto/PizzeriaProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.santiago.pizzeriaProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santiago/pizzeriaProyecto/PizzeriaProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.pizzeriaProyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
