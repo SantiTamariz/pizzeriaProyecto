d2ee170a9c6d26ebfd7178e9dbdafc542c4ea434
Stmarco <S.tmarco@alumnos.upm.es>
2022-06-08 09:43:28 +0200

    CrossOrigin

--- src/main/java/com/santiago/pizzeriaProyecto/Controllers/IngredienteController.java
+++ src/main/java/com/santiago/pizzeriaProyecto/Controllers/IngredienteController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.santiago.pizzeriaProyecto.Entities.Ingrediente;
@@ -28,6 +30,8 @@ import com.santiago.pizzeriaProyecto.Services.IngredienteService;
 
 @RestController
 @RequestMapping("/ingredientes")
+@CrossOrigin(origins = "*", 
+methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class IngredienteController {
     
     @Autowired
